819b7befd569e432f7ed72e96b96ca6e80e76d04
DeltaKhalid <khalid.delta063@gmail.com>
2024-10-29 17:18:10 +0600

    Splash screen's logo set has been done

--- app/src/main/java/com/example/deltav22/SplashScreenActivity.kt
+++ app/src/main/java/com/example/deltav22/SplashScreenActivity.kt
@@ -4,11 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageView
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+
+        // Access the ImageView by its ID
+        val logoImageView: ImageView = findViewById(R.id.logoImageView)
+
+        // Optionally, you can set properties or update the image programmatically
+        logoImageView.setImageResource(R.drawable.logo_delta) // Set image resource if needed
 
 
         // Simulate loading
@@ -17,6 +25,8 @@ class SplashScreenActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 3000) // 3 seconds delay
+
+
 
 
     }
