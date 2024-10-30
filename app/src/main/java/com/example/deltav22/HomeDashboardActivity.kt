5f0783bdd7616ffb88441c6d44f636fa206a2edb
DeltaKhalid <khalid.delta063@gmail.com>
2024-10-30 14:48:23 +0600

    Top Bra design has been done

--- app/src/main/java/com/example/deltav22/HomeDashboardActivity.kt
+++ app/src/main/java/com/example/deltav22/HomeDashboardActivity.kt
@@ -2,10 +2,38 @@ package com.example.deltav22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
 
 class HomeDashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_dashboard)
+
+
+//        val toolbar: Toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+//
+//        // Add drawer icon
+//        toolbar.setNavigationIcon(R.drawable.ic_menu)
+//        toolbar.setNavigationOnClickListener {
+//            // Handle drawer opening logic here
+//        }
+//
+//        // Set page title
+//        toolbar.title = "Delta"
+//
+//        // Add bell icon
+//        val bellIcon = ImageView(this)
+//        bellIcon.setImageResource(R.drawable.ic_menu)
+//        toolbar.addView(bellIcon)
+//
+//        // Add logout icon
+//        val logoutIcon = ImageView(this)
+//        logoutIcon.setImageResource(R.drawable.ic_menu)
+//        toolbar.addView(logoutIcon)
+
+
+
     }
 }
