d7b3fd54583999ab530f60b718f99355d2433f0f
DeltaKhalid <khalid.delta063@gmail.com>
2024-10-31 09:11:02 +0600

    before start Drawar

--- app/src/main/java/com/example/deltav22/HomeDashboardActivity.kt
+++ app/src/main/java/com/example/deltav22/HomeDashboardActivity.kt
@@ -1,14 +1,23 @@
 package com.example.deltav22
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class HomeDashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_dashboard)
+
+        // Set the status bar color
+        //window.statusBarColor = ContextCompat.getColor(this, R.color.GreenMainDark) // Replace with your color
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.pColorShadForStatusBar) // Replace with your color
+        }
 
 
 //        val toolbar: Toolbar = findViewById(R.id.toolbar)
--- app/src/main/java/com/example/deltav22/SplashScreenActivity.kt
+++ app/src/main/java/com/example/deltav22/SplashScreenActivity.kt
@@ -1,15 +1,22 @@
 package com.example.deltav22
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        // code for status bar color change
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        }
 
 
         // Access the ImageView by its ID
