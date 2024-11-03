33d1af7f1c8f8a4286090aaaf9287216f9b37ac0
DeltaKhalid <khalid.delta063@gmail.com>
2024-11-03 11:42:29 +0600

    Login page to go HomeDashboard Activity has been done

--- app/src/main/java/com/example/deltav22/LoginActivity.kt
+++ app/src/main/java/com/example/deltav22/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.example.deltav22
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.id_login_button)
+        loginButton.setOnClickListener {
+            // Start HomeActivity
+            val intent = Intent(this, HomeDashboardActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
+    ///================================================== below OnCreate ============================================///
+
+
+
+
 }
