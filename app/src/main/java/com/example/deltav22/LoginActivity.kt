1d2758a6be3e12af6a2b5b83f7f4a33686bf21ef
DeltaKhalid <khalid.delta063@gmail.com>
2024-11-07 10:01:10 +0600

    before API call start

--- app/src/main/java/com/example/deltav22/LoginActivity.kt
+++ app/src/main/java/com/example/deltav22/LoginActivity.kt
@@ -1,14 +1,21 @@
 package com.example.deltav22
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.content.ContextCompat
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        /// --- code for settings bar color change ( above the ToolBar) ---///
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.white) // Replace with your color
+        }
 
         //--- code for
         val loginButton = findViewById<Button>(R.id.id_login_button)
